cedf506efad8b583492d86c7f5428eed90094801
donovanrichardson <donovanspqa@gmail.com>
2020-05-23 12:54:51 -0400

    graphs 5/22

--- comparisons.R
+++ comparisons.R
@@ -100,7 +100,7 @@ dailyDotLine<-ggplot(sidebyside[-(1:39),], aes(x=date)) +
                       values = pal2) +
   xlab("Date") +
   ylab("Daily Cases") +
-  labs(title = "Daily COVID-19 Cases and Deaths in the US (to May 20)",
+  labs(title = "Daily COVID-19 Cases and Deaths in the US (to May 22)",
        caption = "Points are cases and deaths reported each day.\nLines are 7-day running averages.\nGraph by Donovan Richardson\nData from The New York Times, based on reports from state and local health agencies.") +
   scale_y_log10(breaks=c(1,10,100,1000,10000), minor_breaks=c(5,50,500,5000,50000)) +
   annotation_logticks() +
@@ -125,7 +125,7 @@ dailyNyNj<-ggplot(sidebyside2[-(1:55),], aes(x=date)) +
                       values = pal3) +
   xlab("Date") +
   ylab("Daily Cases") +
-  labs(title = "Daily COVID-19 Deaths in Selected States (to May 20)",
+  labs(title = "Daily COVID-19 Deaths in Selected States (to May 22)",
        caption = "Points are deaths reported each day.\nLines are 7-day running averages.\nGraph by Donovan Richardson\nData from The New York Times, based on reports from state and local health agencies.") +
   # scale_y_log10(breaks=c(1,10,100,1000), minor_breaks=c(5,50,500,5000)) +
   # annotation_logticks() +
@@ -139,3 +139,6 @@ dailyNyNj
 
 ggsave("DailyCases.png", dailyDotLine)
 ggsave("DailyNY-NJ.png", dailyNyNj)
+
+# View(sidebyside$daily_deaths_avg.ny / sidebyside$daily_deaths_avg_usa)
+# View((sidebyside$daily_deaths_avg.ny + sidebyside$daily_deaths_avg.nj) / sidebyside$daily_deaths_avg_usa)
